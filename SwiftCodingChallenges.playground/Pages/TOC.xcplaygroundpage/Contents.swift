0b02e4176a7e4b8ff0e76f0bad7f94282266c868
Jessica Leivers <jessica.leivers@bbc.co.uk>
2017-09-08 10:32:08 +0100

    updated TOC

--- SwiftCodingChallenges.playground/Pages/TOC.xcplaygroundpage/Contents.swift
+++ SwiftCodingChallenges.playground/Pages/TOC.xcplaygroundpage/Contents.swift
@@ -6,6 +6,7 @@
     2. [Are the strings Pallindromes of each other?](Pallindrome)
     3. [Do the strings contain the same characters?](Same%20Characters)
     4. [Does one string contain another?](Custom%20Contains)
+    5. [How many times does the character appear in the string?](Character%20Count)
  
 */
 
